d58aec1ed7810256ad02b186e7ffa3f21bb318da
Елизавета Асеева <aseyshaaa@mail.ru>
2023-12-13 11:30:35 +0700

    create empty menu

--- main.py
+++ main.py
@@ -91,3 +91,24 @@ def view_department_heads(person_list):
             print()
 
 
+def main():
+    while True:
+        print("1. Добавить человека")
+        print("2. Посмотреть список студентов")
+        print("3. Посмотреть список преподавателей")
+        print("4. Посмотреть список заведующих кафедрой")
+        print("5. Выйти")
+        choice = input("Введите ваш выбор: ")
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            break
+        else:
+            break
