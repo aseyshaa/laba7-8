f2ce556f82b1d3303105a1b5762a7cdf290ea212
Елизавета Асеева <aseyshaaa@mail.ru>
2023-12-13 11:27:17 +0700

    create empty classes

---
+++ main.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self):
+        return
+
+
+class Student(Person):
+    def __init__(self):
+        return
+
+
+class Teacher(Person):
+    def __init__(self):
+        return
+
+
+class DepartmentHead(Teacher):
+    def __init__(self):
+        return
